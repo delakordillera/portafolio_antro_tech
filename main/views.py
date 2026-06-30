f90ef6919005862d089c0e22bbc910d059c3daf9
delakordillera <alarvive@gmail.com>
2026-06-29 23:24:48 -0400

    clean: remove temp sync view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from .models import Perfil, Proyecto, Skill
-import os, subprocess
-from django.http import HttpResponse
 
 def home(request):
     perfil = Perfil.objects.first()
@@ -23,11 +21,3 @@ def home(request):
     }
 
     return render(request, 'main/home.html', context)
-
-def git_sync(request):
-    os.chdir('/home/delakordillera/red-apoyo-mutuo')
-    r1 = subprocess.run(['git', 'add', '-A'], capture_output=True, text=True)
-    r2 = subprocess.run(['git', 'commit', '-m', 'sync: merge servidor con GitHub'], capture_output=True, text=True)
-    r3 = subprocess.run(['git', 'push', 'origin', 'main'], capture_output=True, text=True)
-    output = 'add: ' + r1.stdout + r1.stderr + '\ncommit: ' + r2.stdout + r2.stderr + '\npush: ' + r3.stdout + r3.stderr
-    return HttpResponse('<pre>' + output + '</pre>')
